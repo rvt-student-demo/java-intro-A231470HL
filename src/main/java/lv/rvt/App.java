2f08691f85565bdcc97bd4b9462a61cdcbd899d8
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-09-05 09:48:32 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,28 @@
 package lv.rvt;// klases atrašanās vieta projektā
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
-        System.out.println("Sveika pasaule");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ievadi savu vārdu");
+        String name = scanner.nextLine();
+        System.out.println("ievadi savu uzvārdu");
+        String uzvards = scanner.nextLine();
+        System.out.println("ievadi savu grupas nosaukumu");
+        String grupa = scanner.nextLine();
+        //String name = "Jaroslavs";
+        //int age = 17;
+        //boolean isOnline = true;
+
+        System.out.println("Tu ievadīji");
+        System.out.println(name);
+        System.out.println(uzvards);
+        System.out.println(grupa);
+
+
     }
 
 }
