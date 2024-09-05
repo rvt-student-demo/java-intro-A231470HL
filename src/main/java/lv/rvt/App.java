760557c1d6d6933e1f29050b9b69e5770af30ca3
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-09-05 08:08:03 +0000

    pirmais commit

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,11 @@
-package lv.rvt;
+package lv.rvt;// klases atrašanās vieta projektā
 
 public class App 
 {
     public static void main( String[] args )
     {
         // Make your first Hello World app!
-
+        System.out.println("Sveika pasaule");
     }
 
 }
