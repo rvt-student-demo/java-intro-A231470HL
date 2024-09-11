d02783c520bbc6e07d8fa3c29c84e5ccee14d5c2
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-09-11 09:33:30 +0000

    Uzdevums 1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,34 +7,22 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi savu vārdu: ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvārdu: ");
+
+        String lastname = scanner.nextLine();
+
+        System.out.println("ievadi savu grupu: ");
         
-        
-        //System.out.println("ievadi savu vārdu");
-        //String name = scanner.nextLine();
-        //System.out.println("ievadi savu uzvārdu");
-        //String uzvards = scanner.nextLine();
-        //System.out.println("ievadi savu grupas nosaukumu");
-        //String grupa = scanner.nextLine();
-        //String name = "Jaroslavs";
-        //int age = 17;
-        //boolean isOnline = true;
+        String group = scanner.nextLine();
 
-        //System.out.println("Tu ievadīji: "+ name +" "  + uzvards+"asd " + grupa);
-        //System.out.println(name);
-        //System.out.println(uzvards);
-        //System.out.println(grupa);
+        System.out.println("Students: " + name + " " + lastname + ", Grupa: " + group );
 
-        System.out.println("Chicken:");
-        String vista = scanner.nextLine();
-        System.out.println("Bacon(KG):");
-        String bekons = scanner.nextLine();
-        System.out.println("Tractor:");
-        String traktors = scanner.nextLine();
 
-        System.out.println("And finally, a summary");
-        System.out.println(vista);
-        System.out.println(bekons);
-        System.out.println(traktors);
     }
 
 }
