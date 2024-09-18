60d12cff3fbdb74f226c0c06af984aef3cd6952b
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-09-18 08:23:36 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,19 +8,34 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ievadi pirmo skaitli: ");
+        System.out.println("Norādiet punkuts[0-100] ");
 
-        int first = Integer.valueOf(scanner.nextLine());
+        int punkti = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Ievadi otro skaili: ");
-
-        int second = Integer.valueOf(scanner.nextLine());
-
-        System.out.println(first + " + " + second+ " = " + (first+second));
-        System.out.println(first + " - " + second+ " = " + (first-second));
-        System.out.println(first + " * " + second+ " = " + (first*second));
-        System.out.println(first + " / " + second+ " = " + ((double)first/second));
-
+        if (punkti< 0) {
+            System.out.println("Neiespējami");
+        }
+        else if( punkti < 49) {
+            System.out.println("Nenokārtots");
+        }
+        else if( punkti < 59) {
+            System.out.println("1");
+        }
+        else if( punkti < 69) {
+            System.out.println("2");
+        }
+        else if( punkti < 79) {
+            System.out.println("3");
+        }
+        else if( punkti < 89) {
+            System.out.println("4");
+        }
+        else if( punkti < 100) {
+            System.out.println("5");
+        }
+        else if (punkti > 100) {
+            System.out.println("iespaidīgi");
+        }
 
     }
 
