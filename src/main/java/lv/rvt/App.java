5d4fd702faa571651da3fd65ee06865afc4a0c61
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-10-03 08:27:29 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,15 +2,17 @@ package lv.rvt;// klases atrašanās vieta projektā
 
 public class App 
 {
-    public class ExerciseTemplate {
+    public class Main {
         public static void main(String[] args) {
-            printUntilNumber(5);
-            printUntilNumber(2);
+            dalisanaArTris(3, 6);
+            dalisanaArTris(2, 10);
         }
     
-        public static void printUntilNumber(int number) {
-            for (int i = 1; i <= number; i++) {
-                System.out.println(i);
+        public static void dalisanaArTris(int beginning, int end) {
+            for (int i = beginning; i <= end; i++) {
+                if (i % 3 == 0) {
+                    System.out.println(i);
+                }
             }
         }
     }
