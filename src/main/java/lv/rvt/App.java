bf0a5390ad3e49f223710ca59514b6b57739a64c
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-10-03 08:04:41 +0000

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,21 +1,17 @@
 package lv.rvt;// klases atrašanās vieta projektā
 
-import java.util.Scanner;
-
 public class App 
 {
-    public static void main( String[] args )
-    {
-        //Scanner reader = new Scanner(System.in);
-
-       // System.out.println("Enter the value of the gift: ");
-        //int first =Integer.valueOf(reader.nextLine());
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("cik rindas?");
-        int reizes = scanner.nextInt();
-        for (int i = 0; i< reizes; i++){
-            System.out.println("In a hole in the ground there lived a method");
+    public class ExerciseTemplate {
+        public static void main(String[] args) {
+            printUntilNumber(5);
+            printUntilNumber(2);
+        }
+    
+        public static void printUntilNumber(int number) {
+            for (int i = 1; i <= number; i++) {
+                System.out.println(i);
+            }
         }
     }
 }
