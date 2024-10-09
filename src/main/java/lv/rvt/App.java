4053ad3045b2ef60b5e473c85c17fad39c4d3346
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-10-09 09:38:47 +0000

    Printing a square part 2

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,10 +4,7 @@ public class App
 {
     public class Main {
         public static void main(String[] args) {
-           Stars.printStars(5);
-           Stars.printStars(3);
-           Stars.printStars(9);
-
+        Stars.printSquare(4);
         }
     }
 }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -2,10 +2,12 @@ package lv.rvt;
 
 public class Stars {
     
-    public static void printStars( int number){
-        for (int r = 0; r < number; r++){
-            System.out.print("*");
+    public static void printSquare( int size){
+        for (int r = 0; r<size; r++){
+            for (int s = 0; s < size; s++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
 }
