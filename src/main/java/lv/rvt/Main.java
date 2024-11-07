1e2a3e48d17801f902e2db9b223c82aa6ebb0d94
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-11-07 09:02:49 +0000

    count pt2

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,12 +1,16 @@
 package lv.rvt;
 
-public class MainProgram {
+public class Main {
     public static void main(String[] args) {
         Statistics statistics = new Statistics();
         statistics.addNumber(3);
         statistics.addNumber(5);
         statistics.addNumber(1);
         statistics.addNumber(2);
+        
         System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
 }
+
