85fccec3b4f4cd6de3759df5b342a6ed50e121dd
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-11-14 09:09:54 +0000

    Card part 1

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,39 +1,9 @@
 package lv.rvt;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Create three Statistics objects
-        Statistics statistics = new Statistics();
-        Statistics evenStatistics = new Statistics();
-        Statistics oddStatistics = new Statistics();
-
-        System.out.println("Enter numbers:");
-
-        while (true) {
-            int skaitlis = scanner.nextInt();
-
-            if (skaitlis == -1) {
-                break;
-            }
-
-            statistics.addNumber(skaitlis);
-
-            if (skaitlis % 2 == 0) {
-                evenStatistics.addNumber(skaitlis);  
-            } else {
-                oddStatistics.addNumber(skaitlis);   
-            }
-        }
-
-        System.out.println("Sum: " + statistics.getSum());
-        System.out.println("Sum of even numbers: " + evenStatistics.getSum());
-        System.out.println("Sum of odd numbers: " + oddStatistics.getSum());
-
-        scanner.close();  // Close the scanner
+        PaymentCard card = new PaymentCard(50);
+        System.out.println(card);
     }
 }
 
---
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -0,0 +1,14 @@
+package lv.rvt;
+
+public class PaymentCard {
+    private double balance;
+
+
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;
+    }
+
+    public String toString() {
+        return "The card has a balance of " + balance + " euros ";
+    }
+}
