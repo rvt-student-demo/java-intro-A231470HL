3d2edd91cd047e0571a0f077846c278cd342887b
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-11-14 08:39:36 +0000

    Count part 2

---
+++ src/main/java/lv/rvt/Count.java
@@ -0,0 +1,20 @@
+package lv.rvt;
+
+public class Count {
+    private int value;
+
+    public Count (int initialValue) {
+        this.value = initialValue;
+    }
+
+    public void printValue()
+    {
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrement() {
+
+    }
+
+
+}
