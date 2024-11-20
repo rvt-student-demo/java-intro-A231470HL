bbb4adb3147a8fd573cad8592a25e1ef22aee533
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-11-20 10:17:13 +0000

    Payment card pt5

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -3,16 +3,10 @@ package lv.rvt;
 public class Main {
     public static void main(String[] args) {
         PaymentCard card = new PaymentCard(10);
-        System.out.println(card);
+        System.out.println("Paul: " + card);
 
-        card.addMoney(15);  // Add 15 euros
-        System.out.println(card);
-
-        card.addMoney(10);  // Add 10 euros
-        System.out.println(card);
-
-        card.addMoney(200);  // Add 200 euros (will cap the balance at 150)
-        System.out.println(card);
+        card.addMoney(-15);  // Attempt to add a negative amount
+        System.out.println("Paul: " + card);
     }
 }
 
