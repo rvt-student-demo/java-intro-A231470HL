85ccb39de0a1ab28cd71c9ac3bbd889626ad3e15
A231470HL <166109856+A231470HL@users.noreply.github.com>
2024-11-20 10:11:26 +0000

    Payment card pt2

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,9 +1,17 @@
 package lv.rvt;
 
 public class Main {
+
     public static void main(String[] args) {
         PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
+        System.out.println(card);  // Initial balance
+
+        card.eatAffordably();  // Deduct 2.60
+        System.out.println(card);  // After eatAffordably
+
+        card.eatHeartily();  // Deduct 4.60
+        card.eatAffordably();  // Deduct another 2.60
+        System.out.println(card);  // After eatHeartily and second eatAffordably
     }
 }
 
--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -3,12 +3,24 @@ package lv.rvt;
 public class PaymentCard {
     private double balance;
 
-
-    public PaymentCard(double openingBalance) {
-        this.balance = openingBalance;
+    // Constructor that initializes the card with a specific balance
+    public PaymentCard(double initialBalance) {
+        this.balance = initialBalance;
     }
 
+    // Method to reduce balance by 2.60 euros for affordable eating
+    public void eatAffordably() {
+        this.balance -= 2.60;
+    }
+
+    // Method to reduce balance by 4.60 euros for hearty eating
+    public void eatHeartily() {
+        this.balance -= 4.60;
+    }
+
+    // Method to return a string representation of the card's balance
+    @Override
     public String toString() {
-        return "The card has a balance of " + balance + " euros ";
+        return "The card has a balance of " + this.balance + " euros";
     }
 }
