780107807d59123f80294f9b60b7b7ed1daed40a
Haralds Labalaikis <166109856+A231470HL@users.noreply.github.com>
2025-01-15 09:32:38 +0000

    Part 2 student

---
+++ src/main/java/lv/rvt/Student.java
@@ -0,0 +1,34 @@
+package lv.rvt;
+
+public class Student {
+
+    private int credits;
+    private String name;
+    private String address;
+
+    public Student(String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.credits = 0;
+    }
+
+    public void study() {
+        credits++;
+    }
+
+    public int credits() {
+        return credits;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n   "+ address;
+    }
+    public static void main(String[] args) {
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+        System.out.println(ollie);
+        System.out.println("Study credits " + ollie.credits());
+        ollie.study();
+        System.out.println("Study credits " + ollie.credits());
+    }
+}
