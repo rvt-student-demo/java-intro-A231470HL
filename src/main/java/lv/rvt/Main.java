047d891d8828573be8c889e544abf505720a2aca
Haralds Labalaikis <166109856+A231470HL@users.noreply.github.com>
2025-01-28 12:18:29 +0000

    Teacher

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,14 +1,1 @@
-package lv.rvt;
 
-
-
-public class Main {
-    public static void main(String[] args) {
-        Timer timer = new Timer();
-
-        for (int i = 0; i < 500; i++) {
-            System.out.println(timer);
-            timer.advance();
-        }
-    }
-}
--- src/main/java/lv/rvt/Student.java
+++ src/main/java/lv/rvt/Student.java
@@ -1,7 +1,6 @@
 package lv.rvt;
 
 public class Student {
-
     private int credits;
     private String name;
     private String address;
@@ -16,18 +15,47 @@ public class Student {
         credits++;
     }
 
-    public int credits() {
+    public int getCredits() {
         return credits;
     }
 
     @Override
     public String toString() {
-        return name + "\n   "+ address + "\n    " +"Study credits " +credits;
+        return name + "\n   " + address + "\n    " + "Study credits: " + credits;
     }
+}
+
+class Teacher {
+    private String name;
+    private String address;
+    private int salary;
+
+    public Teacher(String name, String address, int salary) {
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n   " + address + "\n    " + "Salary: " + salary;
+    }
+}
+
+class Main {
     public static void main(String[] args) {
+        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+        System.out.println(ada);
+        System.out.println(esko);
+
         Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-        System.out.println(ollie);
-        ollie.study();
+
+        int i = 0;
+        while (i < 25) {
+            ollie.study();
+            i = i + 1;
+        }
         System.out.println(ollie);
     }
 }
